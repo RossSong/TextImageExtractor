2860930cba063af2a3552204566dda21ffa87da9
hgsong <hgsong@wantedlab.com>
2018-01-17 10:15:04 +0900

    Initial Commit

---
+++ TextExtractor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextExtractor
+//
+//  Created by Ross on 2018. 1. 17..
+//  Copyright © 2018년 wanted. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
